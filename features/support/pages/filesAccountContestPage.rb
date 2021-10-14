3c256bebd3c6c07876c9ec0dfe3871b35cedfbce
cramirezUT <carlos.ramirez@unitedtote.com>
2021-10-14 10:59:38 -0700

    Contest class page

--- features/support/pages/filesAccountContestPage.rb
+++ features/support/pages/filesAccountContestPage.rb
@@ -9,7 +9,7 @@ module ContestPage
 	include RSpec::Matchers
 	include Selenium
 
-	#### CONTEST MODAL METHODS / LOCATORS####
+	#### CONTEST MODAL METHODS / LOCATORS ####
 	def contestConfigurationModal
 		@browser.div(id: 'gridcontentFileContests-Select')
 	end
@@ -24,6 +24,39 @@ module ContestPage
 
 	def contestConfigurationHelpButton
 		@browser.button(id: 'FileContests-Help')
+	end
+
+	def contestConfigurationRowSelect
+		@browser.div(id: 'row0FileContests-Select')
+	end
+
+	def contestConfigurationSortAscend
+		@browser.button(id: 'sortascct1FileContests-Selectt')
+	end
+
+	def contestConfigurationSortDescend
+		@browser.button(id: 'sortdescct1FileContests-Select')
+	end
+
+	#### DISPLAYED COLUMNS METHODS / LOCATORS #####
+	def contestDispalyedColumnsModal
+		@browser.button(id: 'GridColumns-Columns')
+	end
+
+	def contestDispalyedColumnsSaveButton
+		@browser.button(id: 'GridColumns-Save')
+	end
+
+	def contestDispalyedColumnsCloseButton
+		@browser.button(id: 'GridColumns-Close')
+	end
+
+	def contestConfigurationRowSelection(index)
+		@browser.div(id: "row#{index}FileContests-Select")
+	end
+
+	def contestDisplayedColumsModalIterateByRows
+		@browser.table(id: "GridColumns-Columns").trs.collect{ |tr| tr[0].text.gsub!(/[\s!@%\/&"']/,'')}
 	end
 
 end
