3895232aff1ee1445e4d30efe272ffd1671896b8
Carlos Ramirez <carlos.ramirez@unitedtote.com>
2021-12-14 21:03:28 -0800

    Added class define and undefine tests

---
+++ features/support/automation/eventClassDefine.rb
@@ -0,0 +1,93 @@
+require_relative "../pages/toteLoginPage.rb"
+require_relative "../pages/homePage.rb"
+require_relative "../pages/siteSelectPage.rb"
+require_relative "../pages/adminEventClassPage.rb"
+require_relative "../lib/browsers.rb"
+require_relative "../lib/SendKeys.rb"
+require_relative "../lib/Users.rb"
+require_relative "../lib/windows.rb"
+
+RSpec.configure do |c|
+  c.include ToteLoginPage
+  c.include HomePage
+  c.include SiteSelectPage
+	c.include Browsers
+  c.include SendKeys
+  c.include ClassPage
+  c.include WindowsHelpers
+end
+
+RSpec.describe "Event Class Define class test", :regression do
+  begin
+    before(:all) do
+      puts "eventClassDefineClassTest"
+      launchToteBrowser
+      selectSiteTable
+      logInFunction
+    end
+
+    after(:each) do |example|
+	    if example.exception
+  	    screenshot_file = "features/support/automation_screenshots/filesAccountBlock-#{Time.now.strftime('%Y%m%d-%H%M%S')}.png"
+  	    @browser.driver.save_screenshot(screenshot_file)
+        sleep 1
+        @browser.quit
+	    end
+	  end
+
+    it "Verifies the main system text in the header menu" do
+      isMainSystemTextDisplayed
+    end
+
+		it "Clicks on the Admin tab in the main menu" do
+			mainMenuLinks("Admin").wait_until_present.flash(color: ["yellow"]).click
+		end
+
+		it "Clicks on the Event tab" do
+      adminMenuLinks("Event").wait_until_present.flash(color: ["yellow"]).click
+		end
+
+    it "Clicks on the Class link" do
+      eventMenuLinks("Class").wait_until_present.flash(color: ["yellow"]).click
+    end
+
+    it "Sets a valid Group ID" do
+      eventClassGroupDropdown.flash(color: ["yellow"]).click
+      eventClassGroupSearchTextField.wait_until_present.flash(color: ["yellow"]).set ($groupId)
+      sendKeysEnter
+      sleep(1)
+      sendKeysTab
+      sendKeysTab
+    end
+
+		it "Defines the event class" do
+			eventClassButtonByIndex(0).wait_until_present.flash(color: ["yellow"]).click
+		end
+
+		it "Clicks on the save changes button" do
+			eventClassSaveChangesButton.wait_until_present.flash(color: ["yellow"]).click
+		end
+
+		it "Verifies a class was defined" do
+			verifyEventClassDefinition(0)
+		end
+
+		it "Reset class definition" do
+			eventClassButtonByIndex(0).wait_until_present.flash(color: ["yellow"]).click
+			eventClassButtonByIndex(0).wait_until_present.flash(color: ["yellow"]).click
+		end
+
+		it "Clicks on the save changes button" do
+			eventClassSaveChangesButton.wait_until_present.flash(color: ["yellow"]).click
+		end
+
+		it "Verifies a class definition was reset" do
+			verifyEventClassDefinitionReset(0)
+		end
+	ensure
+	after(:all) do
+		logOutFuction
+		close_browser
+	end
+	end
+end
