edc4a94a4802cdfadac4a37c95ac2fd031d0c9f8
Carlos Ramirez <carlos.ramirez@unitedtote.com>
2021-12-22 18:56:12 -0800

    Added account cancel test

---
+++ features/support/automation/adminAccountCancel.rb
@@ -0,0 +1,105 @@
+require_relative "../pages/toteLoginPage.rb"
+require_relative "../pages/homePage.rb"
+require_relative "../pages/siteSelectPage.rb"
+require_relative "../pages/adminAccountCancelPage.rb"
+require_relative "../lib/browsers.rb"
+require_relative "../lib/SendKeys.rb"
+require_relative "../lib/Users.rb"
+
+RSpec.configure do |c|
+  c.include ToteLoginPage
+  c.include HomePage
+  c.include SiteSelectPage
+	c.include Browsers
+  c.include SendKeys
+  c.include AccountCancelPage
+end
+
+RSpec.describe "Admin->Account->Cancel: Account ticket cancelation validation", :regression do
+  begin
+    before(:all) do
+      puts "adminAccountCancelTest"
+      launchToteBrowser
+      selectSiteTable
+      logInFunction
+    end
+
+    after(:each) do |example|
+	    if example.exception
+  	    screenshot_file = "features/support/automation_screenshots/filesAccountBlock-#{Time.now.strftime('%Y%m%d-%H%M%S')}.png"
+  	    @browser.driver.save_screenshot(screenshot_file)
+        sleep 1
+        @browser.quit
+	    end
+	  end
+
+    it "Verifies the main system text in the header menu" do
+      isMainSystemTextDisplayed
+    end
+
+		it "Clicks on the Admin tab in the main menu" do
+			mainMenuLinks("Admin").wait_until_present.flash(color: ["yellow"]).click
+		end
+
+		it "Clicks on the Account tab" do
+      adminMenuLinks("Account").wait_until_present.flash(color: ["yellow"]).click
+		end
+
+    it "Clicks on the Handle link" do
+      eventMenuLinks("Cancel").wait_until_present.flash(color: ["yellow"]).click
+    end
+
+    it "Sets the Account Group Number" do
+      accountCancelModal.wait_until_present.flash(color: ["yellow"])
+      accountCancelSearchTextField.wait_until_present.flash(color: ["yellow"]).set ($invalidAccountGroupId)
+      sendKeysEnter
+    end
+
+		it "Verifies the account Cancel error" do
+			verifyEventInformationResultByIndex('A group must be specified! <group id>-<number>')
+		end
+
+		it "Sets a vaild account Cancel group id" do
+			accountCancelModal.wait_until_present.flash(color: ["yellow"])
+			accountCancelSearchTextField.click
+			sendKeysClear
+      accountCancelSearchTextField.flash(color: ["yellow"]).set ($accountGroupId)
+      sendKeysEnter
+		end
+
+		it "Verifies the account Cancel results table" do
+			accountCancelTicketResults.flash(color: ["yellow"])
+		end
+
+		it "Selects a ticket to cancel" do
+			accountCancelTicketResultsByIndex(0).flash(color: ["yellow"]).click
+		end
+
+		it "Clicks on the Cancel Ticket button" do
+			accountCancelTicketButton.flash(color: ["yellow"]).click
+		end
+
+		it "Clicks on the No button in the confirmation modal" do
+			accountCancelConfirmationModal.flash(color: ["yellow"])
+			accountCancelConfirmationModalNoButton.flash(color: ["yellow"]).click
+		end
+
+		it "Selects a ticket to cancel" do
+			accountCancelTicketResultsByIndex(0).flash(color: ["yellow"]).click
+		end
+
+		it "Clicks on the Cancel Ticket button" do
+			accountCancelTicketButton.flash(color: ["yellow"]).click
+		end
+
+		it "Clicks on the Yes button in the confirmation modal" do
+			accountCancelConfirmationModal.flash(color: ["yellow"])
+			accountCancelConfirmationModalYesButton.flash(color: ["yellow"]).click
+		end
+	ensure
+	after(:all) do
+		logOutFuction
+		close_browser
+	end
+	end
+end
---
+++ features/support/pages/adminAccountCancelPage.rb
@@ -0,0 +1,84 @@
+require 'watir'
+require 'rspec'
+require 'pry'
+require 'rubygems'
+require 'selenium-webdriver'
+require 'rspec/expectations'
+
+$accountGroupId = "TST-8683"
+$invalidAccountGroupId = "99"
+
+module AccountCancelPage
+	include RSpec::Matchers
+	include Selenium
+
+	#### Account Cancel METHODS / LOCATORS ####
+	def accountCancelModal
+		@browser.div(id: 'AccountCancel-Select')
+	end
+
+	def accountCancelSearchTextField
+		@browser.text_field(id: 'AccountCancel-Select-Account')
+	end
+
+	def accountCancelRefreshButton
+		@browser.button(id: 'AccountCancel-Select-Refresh')
+	end
+
+	def accountCancelHelpButton
+		@browser.button(id: 'AccountCancel-Select-Help')
+	end
+
+	def accountCancelResultModal
+		@browser.div(id: 'AccountCancel-Amounts')
+	end
+
+	def accountCancelTicketResults
+		@browser.table(id: 'AccountCancel-Tickets')
+	end
+
+	def accountCancelTicketResultsByIndex(index)
+		@browser.button(id: "AccountCancel-Tickets-#{index}-TicketBox-Number")
+	end
+
+	def accountCancelTicketButton
+		@browser.button(id: 'AccountCancel-Cancel')
+	end
+
+	#### Cancel Confirmation Modal ####
+	def accountCancelConfirmationModal
+		@browser.div(id: 'Confirm')
+	end
+
+	def accountCancelConfirmationModalYesButton
+		@browser.button(id: 'Confirm-Yes')
+	end
+
+	def accountCancelConfirmationModalNoButton
+		@browser.button(id: 'Confirm-No')
+	end
+
+	#### GETTERS ####
+	def getAccountCancelError
+		@browser.label(id: 'AccountCancel-Error')
+	end
+
+	def getAccountCancel
+		@browser.label(id: 'AccountCancel-Amounts-Cancel')
+	end
+
+	#### VERIFIERS ####
+	def verifyEventInformationResultByIndex(result)
+		sleep(1)
+		expectedResult = "#{result}"
+		expect(getAccountCancelError.text).to include(expectedResult)
+		getAccountCancelError.flash(color: ["yellow"])
+	end
+
+	def verifyAccountCancelAmount(result)
+		sleep(1)
+		expectedResult = "#{result}"
+		expect(getAccountCancel.text).to include(expectedResult)
+		getAccountCancel.flash(color: ["yellow"])
+	end
+end
