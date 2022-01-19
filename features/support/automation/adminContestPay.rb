c202517086738ee04460bba346c20308f1b6d8c7
Carlos Ramirez <carlos.ramirez@unitedtote.com>
2022-01-18 17:50:38 -0800

    Tests for contest pay

---
+++ features/support/automation/adminContestPay.rb
@@ -0,0 +1,86 @@
+## e2e RSpec/Ruby Test
+## Author: Carlos Ramirez
+## NOTE:
+
+require_relative "../pages/toteLoginPage.rb"
+require_relative "../pages/homePage.rb"
+require_relative "../pages/siteSelectPage.rb"
+require_relative "../pages/adminContestPayPage.rb"
+require_relative "../lib/browsers.rb"
+require_relative "../lib/SendKeys.rb"
+require_relative "../lib/Users.rb"
+
+RSpec.configure do |c|
+  c.include ToteLoginPage
+  c.include HomePage
+  c.include SiteSelectPage
+	c.include Browsers
+  c.include SendKeys
+  c.include ContestPayPage
+end
+
+RSpec.describe "Admin->Contest->Pay: Contest Pay verification test", :regression do
+  begin
+    before(:all) do
+      puts "adminContestPayTest"
+      launchToteBrowser
+      selectSiteTable
+      logInFunction
+    end
+
+    after(:each) do |example|
+	    if example.exception
+  	    screenshot_file = "features/support/automation_screenshots/filesAccountBlock-#{Time.now.strftime('%Y%m%d-%H%M%S')}.png"
+  	    @browser.driver.save_screenshot(screenshot_file)
+        sleep 1
+        @browser.quit
+	    end
+	  end
+
+    it "Verifies the main system text in the header menu" do
+      isMainSystemTextDisplayed
+    end
+
+		it "Clicks on the Admin tab in the main menu" do
+			mainMenuLinks("Admin").wait_until_present.flash(color: ["yellow"]).click
+		end
+
+		it "Clicks on the Contest tab" do
+      adminMenuLinks("Contest").wait_until_present.flash(color: ["yellow"]).click
+		end
+
+		it "Clicks on the Pay link" do
+			eventMenuLinks("Pay").wait_until_present.flash(color: ["yellow"]).click
+		end
+
+		it "Verifies the Contest Pay Modal" do
+			contestPayModal.flash(color: ["yellow"])
+		end
+
+		it "Sets an invalid Player id in search text field" do
+			contestPaySearchTextField.flash(color: ["yellow"]).set ($playerGroupdIdInvalid)
+			sendKeysEnter
+			sendKeysTab
+		end
+
+		it "Verifies the Group must be specified error" do
+			verifyContestPaysError('A group must be specified! <group id>-<number>')
+		end
+
+		it "Sets a vaild Player id" do
+      contestPaySearchTextField.flash(color: ["yellow"]).click
+			sendKeysClear
+      contestPaySearchTextField.flash(color: ["yellow"]).set ($playerGroupdId)
+      sendKeysEnter
+		end
+
+		it "Verifies the Player No outstanding tickets error is displayed" do
+			verifyContestPaysError('No outstanding unpaid tickets found for player TL1-1')
+    end
+	ensure
+	after(:all) do
+		logOutFuction
+		close_browser
+	end
+	end
+end
---
+++ features/support/pages/adminContestPayPage.rb
@@ -0,0 +1,56 @@
+## Class Page Helper File
+## Author: Carlos Ramirez
+
+require 'watir'
+require 'rspec'
+require 'pry'
+require 'rubygems'
+require 'selenium-webdriver'
+require 'rspec/expectations'
+
+$playerGroupdId = "TL1-1"
+$playerGroupdIdInvalid = "TNT"
+
+module ContestPayPage
+	include RSpec::Matchers
+	include Selenium
+
+	#### CONTEST PAY METHODS / LOCATORS ####
+	def contestPayModal
+		@browser.div(id: 'ContestPay-Select')
+	end
+
+	def contestPayHelpButton
+		@browser.button(id: 'ContestPay-Select-Help')
+	end
+
+  def contestPaySearchTextField
+    @browser.text_field(id: 'ContestPay-Select-Account')
+  end
+	##########################################
+
+	#### GETTERS ####
+	def getContestPayError
+		@browser.label(id: 'ContestPay-Error')
+	end
+
+	#### VERIFIERS ####
+	def verifyContestPaysError(result)
+		sleep(1)
+		expectedResult = "#{result}"
+		expect(getContestPayError.text).to include(expectedResult)
+		getContestPayError.flash(color: ["yellow"])
+	end
+
+	def verifyContestPayModal
+		sleep(1)
+		expect(contestPayTournamentModal).to be_truthy
+		contestPayTournamentModal.flash(color: ["yellow"])
+	end
+
+	def verifyContestPayContestModal
+		sleep(1)
+		expect(contestPayContestModal).to be_truthy
+		contestPayContestModal.flash(color: ["yellow"])
+	end
+end
